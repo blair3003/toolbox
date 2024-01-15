81c291f4ca223460813c7340d78cbae31d825190
Blair Fleming <blair3003.bf@gmail.com>
2024-01-15 19:10:17 +0000

    Header style

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
         <header className="bg-stone-800">
             <div className="lg:max-w-screen-xl lg:mx-auto flex gap-2 lg:gap-4 p-1">
                 <div className="flex items-center justify-around gap-2 lg:gap-4 basis-1/5 shrink-0 my-auto">
-                    <div className="max-w-40">
+                    <div className="p-1 max-w-40">
                         <HomeLink />
                     </div>
                     <div className="hidden lg:grid place-content-center">

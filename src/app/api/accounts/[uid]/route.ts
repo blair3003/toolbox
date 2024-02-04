56fae922f2a512e0ea365107552305c8994232ec
Blair Fleming <blair3003.bf@gmail.com>
2024-02-04 14:53:15 +0000

    removed api

--- src/app/api/accounts/[uid]/route.ts
+++
@@ -1,8 +0,0 @@
-import getAccountFromUID from '@/lib/getAccountFromUID'
-
-export async function GET(request: Request, { params }: { params: { uid: string } }) {
-    const uid = params.uid
-    const account = await getAccountFromUID(uid)
-   
-    return Response.json(account)
-}

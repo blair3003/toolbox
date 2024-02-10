95c96cf4c142cc076fe092d220d860b87102adf6
Blair Fleming <blair3003.bf@gmail.com>
2024-02-10 13:02:25 +0000

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -17,8 +17,8 @@ const HomePage = ({ products }: HomePageProps) => {
             <div className="lg:max-w-screen-xl lg:mx-auto">
                 <section className="p-2 lg:p-4">
                     <header className="p-2 lg:p-4 mb-2 lg:mb-4 text-left lg:text-center">
-                        <h1 className={`mb-2 lg:mb-4 text-xl lg:text-3xl uppercase text-orange-600 ${bigShoulders.className}`}>Home Improvement</h1>
-                        <p className="text-xs lg:text-base">The proof is in the pudding</p>
+                        <h1 className={`mb-2 lg:mb-4 text-xl lg:text-3xl uppercase text-orange-600 ${bigShoulders.className}`}>Every Tool. Every Project.</h1>
+                        <p className="text-xs lg:text-base">Welcome to Toolbox!</p>
                     </header>
                     {products?.length ? <ProductList products={products} /> : null}
                 </section>
